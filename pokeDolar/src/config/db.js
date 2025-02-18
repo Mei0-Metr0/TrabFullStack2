f4549d8e87899d7f9bee44808e51f2c20eb4cb89
Mei0-Metr0 <meiometro.mail@gmail.com>
2025-02-18 15:29:52 -0300

    feat: Add Pool de Conexões no BD

--- pokeDolar/src/config/db.js
+++ pokeDolar/src/config/db.js
@@ -7,7 +7,12 @@ const connectDB = async () => {
         console.log('Connecting to MongoDB');
 
         // Conecta ao MongoDB
-        await mongoose.connect(uri);
+        await mongoose.connect(uri, {
+            maxPoolSize: 10, // tamanho máximo do pool de conexões, 10 conexões simultâneas podem ser usadas.
+            minPoolSize: 2, // número mínimo de conexões que devem ser mantidas ativas, garantindo que sempre haja pelo menos 2 conexões prontas.
+            serverSelectionTimeoutMS: 5000, // Tempo máximo (5 segundos) que o Mongoose esperará para encontrar um servidor MongoDB disponível antes de lançar um erro.
+            socketTimeoutMS: 45000, // Tempo máximo (45 segundos) que o driver esperará por uma resposta do banco de dados antes de considerar a conexão falha.
+        });
         
         console.log('MongoDB connected');
     } catch (error) {
