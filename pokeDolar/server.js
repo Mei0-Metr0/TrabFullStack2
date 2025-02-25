866e135959c171851bf543cce6e4f5e2850ebd7b
Mei0-Metr0 <163468366+Mei0-Metr0@users.noreply.github.com>
2025-02-24 23:40:19 -0300

    refactor: Add URL Cors

--- pokeDolar/server.js
+++ pokeDolar/server.js
@@ -105,7 +105,7 @@ app.use(session({
 
 // Configuração CORS
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173', 'https://trab-full-stack2-26j9eo28u-joice-kellys-projects.vercel.app'],
   credentials: true // Importante para sessões
 }));
 
